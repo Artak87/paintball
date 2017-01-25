a1d6712c9d7b900c6d28c4813995ecb50126ca56
Artak Mnatsakanyan <artak.mnatsakanyan87@gmail.com>
2017-01-25 22:10:10 +0400

    Create function if click time of span add in time start input

--- public/js/choose-time.js
+++ public/js/choose-time.js
@@ -3,5 +3,15 @@ jQuery(document).ready(function ($) {
     $(".time-list li span").click(function () {
         var element = $(this);
         element.removeClass("badge-success").addClass("badge-primary");
-    })
+        console.log(element);
+        $('span').click(function(){
+            var t = $(this).text();
+            // alert(t);
+            $("#startTime").val(t);
+        });
+
+    });
+
+
+
 });

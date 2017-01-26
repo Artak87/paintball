9dc3be540473098b0798d676bbebceefe9a7a2a9
Artak Mnatsakanyan <artak.mnatsakanyan87@gmail.com>
2017-01-26 16:46:30 +0400

    Add function calcualtePrice.

--- public/js/choose-time.js
+++ public/js/choose-time.js
@@ -1,19 +1,33 @@
 jQuery(document).ready(function ($) {
 
+    var duration = 0;
+
     $(".time-list li span").click(function () {
         var element = $(this);
 
         if (element.hasClass("badge-primary")) {
             element.removeClass("badge-primary").addClass("badge-success");
             $("#startTime").val("");
+            duration -= 0.5;
         } else {
             element.removeClass("badge-success").addClass("badge-primary");
             var t = element.text();
             $("#startTime").val(t);
+            duration += 0.5;
 
         }
         //console.log(element);
+        $("#duration").val(duration);
     });
+
+    function calcualtePrice(playersNumber, duration) {
+        var x, y, z;
+        x = playersNumber;
+        y = duration;
+        z = x * (y * 20);
+        return z;
+    }
+
 
     $("#show").click(function () {
         $(".contactInformationGroup").show(1000);

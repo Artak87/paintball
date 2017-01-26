2ae30484fe71550fa362c78892e1b8f2341b48a4
Artak Mnatsakanyan <artak.mnatsakanyan87@gmail.com>
2017-01-26 12:09:21 +0400

    Create function if click + or - start add show or hide

--- public/js/choose-time.js
+++ public/js/choose-time.js
@@ -2,14 +2,24 @@ jQuery(document).ready(function ($) {
 
     $(".time-list li span").click(function () {
         var element = $(this);
+
         element.removeClass("badge-success").addClass("badge-primary");
-        console.log(element);
-        $('span').click(function(){
-            var t = $(this).text();
+        //console.log(element);
+            var t = element.text();
             // alert(t);
             $("#startTime").val(t);
+        $(".time-list li span").click(function () {
+            $(this).removeClass("badge-primary").addClass("badge-success");
         });
 
+    });
+
+    $("#show").click(function () {
+        $(".contactInformationGroup").show(1000);
+    });
+
+    $("#hide").click(function () {
+        $(".contactInformationGroup").hide(1000);
     });
 
 
